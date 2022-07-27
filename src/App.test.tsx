cb7c9ffc3a6707e7093bd24b1225f9939f750657
Anderboi <Upolovnew@gmail.com>
2022-07-27 13:54:57 +0300

    code cleaned

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import { ITask } from "./components/TaskBlock/TaskBlock.props";
 
-const taskList: Array<ITask> = [
-    { taskName: "First", isDone: false, taskId: 1 },
-    { taskName: "Second", isDone: false, taskId: 2 },
-    { taskName: "Third", isDone: true, taskId: 3 },
-    { taskName: "Fourth", isDone: false, taskId: 4 },
-];
 
 describe("main functionality tests", () => {
     test("renders learn react link", () => {
--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -1,32 +1,12 @@
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Input } from "./Input";
-import { IInput } from "./Input.props";
 import userEvent from "@testing-library/user-event";
-
-
 
 const onChange = jest.fn();
 const onSubmit = jest.fn();
 
 describe("Input block functions tests", () => {
-    // test("New task should be added", () => {
-    //     render(<Input handleSubmit={onSubmit} value="" onChange={onChange} />);
-    //     const input = screen.getByTestId(/input/i);
-    //     const form = screen.getByTestId(/form/i);
-
-    //     expect(input).toBeInTheDocument();
-    //     expect(form).toBeInTheDocument();
-
-    //     fireEvent.change(input, { target: { value: "Test" } });
-    //     expect(screen.getByText(/test/i)).toBeInTheDocument();
-    //     fireEvent.keyDown(form, {
-    //         key: "Enter",
-    //         code: "Enter",
-    //         charCode: 13,
-    //     });
-
-    // });
     test("custom placeholder added", () => {
         render(
             <Input
